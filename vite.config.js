22809cdcf310b248b2bb3a1527ed3c7e18a72884
Ahmad Tsaqif Al Arifin <tsaqifcakep87@gmail.com>
2025-12-06 17:29:15 +0700

    fix - pwa not working

--- vite.config.js
+++ vite.config.js
@@ -22,10 +22,18 @@ export default defineConfig({
                 display: "standalone",
                 start_url: "/",
                 icons: [
-                    { src: "/icons/icon-192.png", sizes: "192x192", type: "image/png" },
-                    { src: "/icons/icon-512.png", sizes: "512x512", type: "image/png" }
-                ]
-            }
-        })
+                    {
+                        src: "/storage/icons/icon-192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/storage/icons/icon-512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                ],
+            },
+        }),
     ],
 });
